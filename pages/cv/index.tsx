23d1e9da786ea6e87f6bbcab8748006784ae2adc
Caroline Shaw <caroline.shaw@gmail.com>
2023-03-13 21:38:10 +0000

    Update CV

--- pages/cv/index.tsx
+++ pages/cv/index.tsx
@@ -17,7 +17,7 @@ export default function CV() {
             className="cv"
             width="100%"
             id="cv"
-            src="/assets/cvs/corey-robinson-cv-usa-september-2022.pdf"
+            src="/assets/cvs/corey-robinson-cv-march-2023.pdf"
           ></iframe>
         </Container>
       </Layout>
